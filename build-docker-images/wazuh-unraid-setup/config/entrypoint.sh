a6836d228447d0462357c917cba6cab3fcaf1489
Nathan Price <gravityfargo@gmail.com>
2024-07-08 10:28:24 -0400

    wazuh-dashboard-config dir

--- build-docker-images/wazuh-unraid-setup/config/entrypoint.sh
+++ build-docker-images/wazuh-unraid-setup/config/entrypoint.sh
@@ -10,11 +10,13 @@ rm -rf /wazuh-certificates
 mkdir -p /wazuh-indexer/certs
 mkdir -p /wazuh-manager/filebeat-etc
 mkdir -p /wazuh-manager/ossec-api-configuration/ssl
-mkdir -p /wazuh-dashboard
+mkdir -p /wazuh-dashboard/certs
+mkdir -p /wazuh-dashboard/wazuh-dashboard-config
 
 cp /config/certs.yml /config.yml
+# cp /config/certs.yml /wazuh-indexer/certs.yml
 cp /config/opensearch_dashboards.yml /wazuh-dashboard/opensearch_dashboards.yml
-cp /config/wazuh.yml /wazuh-dashboard/wazuh.yml
+cp /config/wazuh.yml /wazuh-dashboard/wazuh-dashboard-config/wazuh.yml
 cp /config/internal_users.yml /wazuh-indexer/internal_users.yml
 cp /config/indexer.yml /wazuh-indexer/indexer.yml
 cp /config/wazuh_manager.conf /wazuh-manager/ossec.conf
@@ -71,13 +73,16 @@ node_names=($nodes_server)
 
 echo "Moving created certificates to the destination directory"
 cp /wazuh-certificates/* /wazuh-indexer/certs/
-cp /wazuh-indexer/certs/root-ca.pem /wazuh-indexer/certs/root-ca-manager.pem
-cp /wazuh-indexer/certs/root-ca.key /wazuh-indexer/certs/root-ca-manager.key
+# cp /wazuh-indexer/certs/root-ca.pem /wazuh-indexer/certs/root-ca-manager.pem
+# cp /wazuh-indexer/certs/root-ca.key /wazuh-indexer/certs/root-ca-manager.key
 
 mv /wazuh-indexer/certs/wazuh-indexer-key.pem /wazuh-indexer/certs/wazuh.indexer.key
 mv /wazuh-indexer/certs/wazuh-indexer.pem /wazuh-indexer/certs/wazuh.indexer.pem
 mv /wazuh-indexer/certs/wazuh-manager.pem /wazuh-indexer/certs/filebeat.pem
 mv /wazuh-indexer/certs/wazuh-manager-key.pem /wazuh-indexer/certs/filebeat.key
+
+mv /wazuh-indexer/certs/wazuh-dashboard-key.pem /wazuh-dashboard/certs/wazuh-dashboard-key.pem
+mv /wazuh-indexer/certs/wazuh-dashboard.pem /wazuh-dashboard/certs/wazuh-dashboard.pem
 
 # chown 999:999  /wazuh-indexer/certs/root-ca-manager.pem
 # chown 999:999  /wazuh-indexer/certs/root-ca-manager.key
@@ -85,6 +90,10 @@ mv /wazuh-indexer/certs/wazuh-manager-key.pem /wazuh-indexer/certs/filebeat.key
 chmod -R 500 /wazuh-indexer/certs
 chmod -R 400 /wazuh-indexer/certs/*
 chown 1000:1000 /wazuh-indexer/certs/*
+
+chmod 500 /wazuh-dashboard/certs
+chmod 400 /wazuh-dashboard/certs/*
+chown 1000:1000 /wazuh-dashboard/certs/*
 
 echo "Certificates created successfully"
 echo "wazuh-unraid-setup: Finished!"
